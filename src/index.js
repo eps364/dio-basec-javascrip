9484da66c7b295377f54d46b69bb7f7c40fbdee3
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2023-11-25 23:20:50 -0300

    Adicionando todos os testes por XP

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 let name = "Super";
-let xp = 1000;
+let xp = 0;
 let nivel = "Ferro";
 
-if (xp <= 2000) nivel = "Ferro";
-else if (xp > 2000) nivel = "Bronze";
-else if (xp > 3000) nivel = "Prata";
-else if (xp > 4000) nivel = "Ouro";
-else if (xp > 5000) nivel = "Platina";
-else if (xp > 6000) nivel = "Ascendente";
-else if (xp > 7000) nivel = "Imortal";
-else xp = "Bronze";
+for (i = 0; i < 10; i++) {
+  xp = i * 1000;
 
-console.log(
-  "O Herói de nome " +
-    name +
-    " está no nível " +
-    nivel +
-    " com " +
-    xp +
-    " de XP"
-);
+  if (xp <= 1000) nivel = "Ferro";
+  else if (xp <= 2000) nivel = "Bronze";
+  else if (xp <= 3000) nivel = "Prata";
+  else if (xp <= 4000) nivel = "Ouro";
+  else if (xp <= 5000) nivel = "Platina";
+  else if (xp <= 6000) nivel = "Ascendente";
+  else if (xp <= 7000) nivel = "Imortal";
+  else if (xp <= 8000) nivel = "Radiante";
+  ("Bronze");
+
+  console.log(
+    "Teste: O Herói de nome " +
+      name +
+      " está no nível " +
+      nivel +
+      " com " +
+      xp +
+      " de XP"
+  );
+}
